0ba439cf02c1304aab22f85fade3cd89a47c3cb3
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-09-06 21:19:52 +0530

    Add files via upload

---
+++ numpy.py
@@ -0,0 +1,32 @@
+import numpy as np
+a = np.array([[1,2],[3,4]])
+b = np.array([[5,6],[7,8]])
+print(a)
+print(b)
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+# - O/P
+'''
+[[1 2]
+ [3 4]]
+[[5 6]
+ [7 8]]
+[[ 6  8]
+ [10 12]]
+[[-4 -4]
+ [-4 -4]]
+[[ 5 12]
+ [21 32]]
+[[0.2        0.33333333]
+ [0.42857143 0.5       ]]
+'''
+
+#0-D ARRAY
+import numpy as np
+arr = np.array(42)
+print(arr)
+
+# 42
